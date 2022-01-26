ab3bff33cf0f1f1ddbfd0aaf53f8f188a21b8215
tancalv1 <CalvinWSTan37@gmail.com>
2022-01-26 16:09:41 +0000

    Login view and url updated

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     # path for registration
 
     # path for login
-
+    path(route='login/', view=views.login_request, name='login'),
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -32,8 +32,21 @@ def contact(request):
     context = {}
     return render(request, 'djangoapp/contact.html', context)
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
